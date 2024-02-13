6d9829b2a07f27037bf3c403840a5c38fc07066e
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-02-14 06:10:19 +1030

    Create basic add activity view

---
+++ ActivityTracker/AddActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  AddActivityView.swift
+//  ActivityTracker
+//
+//  Created by Woodrow Martyr on 13/2/2024.
+//
+
+import SwiftUI
+
+struct AddActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddActivityView()
+}
