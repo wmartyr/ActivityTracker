447e4d97ddb28680259eeac3347c988892b808c4
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-02-22 06:54:28 +1030

    Add delete functionality to list

---
+++ ActivityTracker/ActivityDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityDetailView.swift
+//  ActivityTracker
+//
+//  Created by Woodrow Martyr on 20/2/2024.
+//
+
+import SwiftUI
+
+struct ActivityDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityDetailView()
+}
